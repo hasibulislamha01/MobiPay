e912a409bca44314ef0cf845175eadaeb2cf3915
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-08-14 13:59:20 +0600

    balance inquiry page added

--- src/main.jsx
+++ src/main.jsx
@@ -11,14 +11,11 @@ import Login from './Pages/LoginPage/Login';
 import Register from './Pages/RegisterPage/Register';
 import AuthProvider from './Components/SharedComponents/Navbar/Auth/AuthProvider';
 import ManageUsers from './Pages/AdminPages/ManageUsers/ManageUsers';
-<<<<<<< HEAD
+
 import SystemMonitor from './Pages/AdminPages/SystemMontor/SystemMonitor';
 import BalanceInquiry from './Pages/UserPages/BalanceInquiryPage/BalanceInquiry';
-=======
-import SendMoney from './Pages/UsersPages/SendMoney';
-import CashIn from './Pages/UsersPages/CashInPage/CashIn';
-import CashOut from './Pages/UsersPages/CashOutPage/CashOut';
->>>>>>> 266f0f99da00a29331182b3526a73cff91f5ebab
+
+
 
 const router = createBrowserRouter([
   {
@@ -43,7 +40,7 @@ const router = createBrowserRouter([
         path: "/user-management",
         element: <ManageUsers></ManageUsers>
       },
-<<<<<<< HEAD
+
       {
         path: "/system-monitoring",
         element: <SystemMonitor></SystemMonitor>
@@ -69,21 +66,7 @@ const router = createBrowserRouter([
       {
         path: '/send-money',
         element: <BalanceInquiry></BalanceInquiry>
-=======
 
-      // User routes
-      {
-        path: "/send-money",
-        element: <SendMoney></SendMoney>
-      },
-      {
-        path: "/cash-in",
-        element: <CashIn></CashIn>
-      },
-      {
-        path: "/cash-out",
-        element: <CashOut></CashOut>
->>>>>>> 266f0f99da00a29331182b3526a73cff91f5ebab
       },
     ]
   },
